30133ee71682f9eee17ef7ae49a5e59288f450b9
cuevrob <rcueva@kruger.com.ec>
2018-10-03 10:39:09 -0500

    habilitar transacciones - Roberto Cueva

--- src/app/component/AcreditarMillas/AcreditarMillas.component.ts
+++ src/app/component/AcreditarMillas/AcreditarMillas.component.ts
@@ -16,14 +16,27 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { AcreditarMillasService } from './AcreditarMillas.service';
 import 'rxjs/add/operator/toPromise';
+import { ViewEncapsulation } from '@angular/core';
+import {NgbModal, ModalDismissReasons, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-acreditarmillas',
   templateUrl: './AcreditarMillas.component.html',
   // styleUrls: ['./AcreditarMillas.component.scss'],
-  providers: [AcreditarMillasService]
+  providers: [AcreditarMillasService],
+  styles: [`
+    .dark-modal .modal-content {
+      background-color: #009efb;
+      color: white;
+    }
+    .dark-modal .close {
+      color: white;   
+    }
+  `]
 })
 export class AcreditarMillasComponent implements OnInit {
+  closeResult: string;
 
   myForm: FormGroup;
 
@@ -41,7 +54,7 @@ export class AcreditarMillasComponent implements OnInit {
   timestamp = new FormControl('', Validators.required);
 
 
-  constructor(private serviceAcreditarMillas: AcreditarMillasService, fb: FormBuilder) {
+  constructor(private serviceAcreditarMillas: AcreditarMillasService, fb: FormBuilder,private modalService: NgbModal, private modalService2: NgbModal) {
     this.myForm = fb.group({
       billetera: this.billetera,
       valor: this.valor,
@@ -55,6 +68,26 @@ export class AcreditarMillasComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadAll();
+  }
+
+  open2(content) { 
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  open(content) {
+    this.modalService2.open(content, { windowClass: 'dark-modal' });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
   loadAll(): Promise<any> {
--- src/app/component/CobrarMillas/CobrarMillas.component.ts
+++ src/app/component/CobrarMillas/CobrarMillas.component.ts
@@ -16,16 +16,29 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { CobrarMillasService } from './CobrarMillas.service';
 import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/toPromise';
+import { ViewEncapsulation } from '@angular/core';
+import {NgbModal, ModalDismissReasons, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-cobrarmillas',
   templateUrl: './CobrarMillas.component.html',
-  providers: [CobrarMillasService]
+  providers: [CobrarMillasService],
+  styles: [`
+  .dark-modal .modal-content {
+    background-color: #009efb;
+    color: white;
+  }
+  .dark-modal .close {
+    color: white;   
+  }
+`]
 })
 export class CobrarMillasComponent implements OnInit {
+  closeResult: string;
 
   myForm: FormGroup;
-
+  
   private allTransactions;
   private Transaction;
   private currentId;
@@ -39,7 +52,7 @@ export class CobrarMillasComponent implements OnInit {
   timestamp = new FormControl('', Validators.required);
 
 
-  constructor(private serviceCobrarMillas: CobrarMillasService, fb: FormBuilder) {
+  constructor(private serviceCobrarMillas: CobrarMillasService, fb: FormBuilder,private modalService: NgbModal, private modalService2: NgbModal) {
     this.myForm = fb.group({
       billetera: this.billetera,
       valorCobro: this.valorCobro,
@@ -52,6 +65,26 @@ export class CobrarMillasComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadAll();
+  }
+
+  open2(content) { 
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  open(content) {
+    this.modalService2.open(content, { windowClass: 'dark-modal' });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
   loadAll(): Promise<any> {
